2022c682fe8425450fe8dbf5371e4a57bb4e807f
ak-felix <47936818+ak-felix@users.noreply.github.com>
2023-06-01 21:31:51 +0530

    Sample Maven project

---
+++ src/main/java/sample_maven_project/sample_hello_world/App.java
@@ -0,0 +1,13 @@
+package sample_maven_project.sample_hello_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
